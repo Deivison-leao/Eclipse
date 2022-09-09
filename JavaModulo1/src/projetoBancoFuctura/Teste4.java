0cc0bc0d9e69e74fc8898b4760cca8a27544f221
lima Deivison <limadleao@gmail.com>
2022-09-09 00:05:03 -0300

    Teste Commit 5

---
+++ JavaModulo1/src/projetoBancoFuctura/Teste4.java
@@ -0,0 +1,12 @@
+package projetoBancoFuctura;
+
+import java.util.Scanner;
+
+public class Teste4 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+	}
+
+}
